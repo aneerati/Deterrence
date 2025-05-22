d305603b1e16ac0e3dc43eca9c83dbc2bbcace1a
aneerati <aneerati2004@gmail.com>
2025-05-21 22:11:29 -0400

    simple player character on screen

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -2,6 +2,8 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
@@ -32,11 +34,28 @@ public class GamePanel extends JPanel implements Runnable {
 
     @Override
     public void run() {
-        // 1. Update info in game
+        // Game loop does 2 things:
+        // 1. Update info in game (e.g. player position)
         // 2. Draw screen with updated info
         while (gameThread != null) {
-            System.out.println("Game Loop running");
-
+            update();
+            repaint();
         }
+    }
+
+    public void update() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.WHITE);
+
+        // player character for now
+        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.dispose();
     }
 }
