aae0f114dbf9d2d02c72de9233b020bab8c1697d
aneerati <aneerati2004@gmail.com>
2025-05-21 21:30:14 -0400

    window stuff

---
+++ src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        // Copied all this shit; don't know how it works
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // prob make this true at some point
+        window.setResizable(false);
+        window.setTitle("Deterrence");
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
