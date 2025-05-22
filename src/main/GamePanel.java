e8b9af7385f6322f0987dd6df7604d18dacf8894
aneerati <aneerati2004@gmail.com>
2025-05-21 22:02:18 -0400

    created basic game loop

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -5,7 +5,7 @@ import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     // Screen params
     final int originalTileSize = 16; // 16x16
@@ -17,9 +17,26 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+    Thread gameThread;
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        // 1. Update info in game
+        // 2. Draw screen with updated info
+        while (gameThread != null) {
+            System.out.println("Game Loop running");
+
+        }
     }
 }
