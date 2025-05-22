e2e18fbb3221c5b392f7f19f8d8e741c902ddd92
aneerati <aneerati2004@gmail.com>
2025-05-22 14:07:50 -0400

    added moving animations

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,16 +1,12 @@
 package entity;
 
-import main.KeyHandler;
-
-import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
 
 import main.GamePanel;
+import main.KeyHandler;
 
 public class Player extends Entity {
 
@@ -32,6 +28,7 @@ public class Player extends Entity {
     }
 
     public void getPlayerImage() {
+        // loads all sprites once
         try {
             up1 = ImageIO.read(getClass().getResourceAsStream("/player/player_up_1.png"));
             up2 = ImageIO.read(getClass().getResourceAsStream("/player/player_up_2.png"));
@@ -64,6 +61,19 @@ public class Player extends Entity {
             direction = "right";
             x = x + speed;
         }
+
+        if (keyH.upPressed == true || keyH.downPressed == true || keyH.leftPressed == true
+                || keyH.rightPressed == true) {
+            spriteCounter++;
+            if (spriteCounter > 10) {
+                if (spriteNum == 1) {
+                    spriteNum = 2;
+                } else if (spriteNum == 2) {
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
+        }
     }
 
     public void draw(Graphics2D g2) {
@@ -71,16 +81,36 @@ public class Player extends Entity {
 
         switch (direction) {
             case "up":
-                image = up1;
+                if (spriteNum == 1) {
+                    image = up1;
+                }
+                if (spriteNum == 2) {
+                    image = up2;
+                }
                 break;
             case "down":
-                image = down1;
+                if (spriteNum == 1) {
+                    image = down1;
+                }
+                if (spriteNum == 2) {
+                    image = down2;
+                }
                 break;
             case "left":
-                image = left1;
+                if (spriteNum == 1) {
+                    image = left1;
+                }
+                if (spriteNum == 2) {
+                    image = left2;
+                }
                 break;
             case "right":
-                image = right1;
+                if (spriteNum == 1) {
+                    image = right1;
+                }
+                if (spriteNum == 2) {
+                    image = right2;
+                }
                 break;
             default:
                 break;
