6d82a34d60a1e496f9b70efe5fa50f2779fd82f4
aneerati <aneerati2004@gmail.com>
2025-05-26 21:35:53 -0400

    created object super class

---
+++ src/object/SuperObject.java
@@ -0,0 +1,13 @@
+package object;
+
+import java.awt.image.BufferedImage;
+
+public class SuperObject {
+
+    public BufferedImage image;
+    public String name;
+    public boolean collision = false;
+    public int worldX;
+    public int worldY;
+
+}
