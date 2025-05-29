074c98203ba5fecf5856781e690e9305d5bfe6c7
aneerati <aneerati2004@gmail.com>
2025-05-28 21:09:06 -0400

    fixed effect/music interference bug

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -37,7 +37,8 @@ public class GamePanel extends JPanel implements Runnable {
     KeyHandler keyH = new KeyHandler();
     TileManager tileM = new TileManager(this);
     public AssetSetter aSetter = new AssetSetter(this);
-    Sound sound = new Sound();
+    Sound music = new Sound();
+    Sound soundEffect = new Sound();
     Thread gameThread;
 
     // ENTITY and OBJECT CLASSES
@@ -119,17 +120,17 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void playMusic(int i) {
-        sound.setFile(i);
-        sound.play();
-        sound.loop();
+        music.setFile(i);
+        music.play();
+        music.loop();
     }
 
     public void stopMusic() {
-        sound.stop();
+        music.stop();
     }
 
     public void playSoundEffect(int i) {
-        sound.setFile(i);
-        sound.play();
+        soundEffect.setFile(i);
+        soundEffect.play();
     }
 }
