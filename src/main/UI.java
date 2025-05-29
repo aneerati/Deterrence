127238e6d4acb7f0bfeeba26d5bb45094d33a060
aneerati <aneerati2004@gmail.com>
2025-05-28 21:23:31 -0400

    improved UI display to have images

--- src/main/UI.java
+++ src/main/UI.java
@@ -3,21 +3,28 @@ package main;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import object.OBJ_Key;
 
 public class UI {
 
     GamePanel gp;
     Font textFont;
+    BufferedImage keyImage;
 
     public UI(GamePanel gp) {
         this.gp = gp;
         this.textFont = new Font("Arial", Font.PLAIN, 40);
+        OBJ_Key key = new OBJ_Key();
+        keyImage = key.image;
     }
 
     public void draw(Graphics2D g2) {
 
         g2.setFont(textFont);
         g2.setColor(Color.WHITE);
-        g2.drawString("Key = " + gp.player.hasKey, 50, 50);
+        g2.drawImage(keyImage, gp.tileSize / 2, gp.tileSize / 2, gp.tileSize, gp.tileSize, null);
+        g2.drawString(": " + gp.player.hasKey, 74, 65);
     }
 }
